ccedf24a511d241a48452b7f5e4a77a602e88340
Luke Plausin <luke.plausin@gmail.com>
2019-10-17 09:42:03 +0100

    Now works asynchronously; ;

--- s3du/__init__.py
+++ s3du/__init__.py
@@ -52,19 +52,24 @@ class Prefix():
 
 
 class S3Counter():
-    def __init__(self, prefix='', separator='/', depth=-1):
+    def __init__(self, prefix='', separator='/', depth=-1, limit=5):
         # Start with basic counter
         self.separator = separator
         self.prefix = prefix
         self.depth = depth
+        self.limit = limit
         self.current_prefix = separator + prefix
         self.counters = [Prefix(key=prefix)]
 
     def report(self, counter):
         # Report a counter which has finished counting
-        print("{size:>16}B  N: {count:>13} {key:>60}".format(
+        print("{size:>16} B  {count:>13} {key:>60}".format(
                 size=counter.size, count=counter.number_objects, key=counter.key))
 
+    def report_omission(self):
+        print("                         additional objects under \"{}\" omitted...".format(
+            self.counters[-1].key
+        ))
 
     def _compare_prefixes(self, l_prefix, r_prefix):
         # Check if prefixes are 'equal' to the given depth
@@ -81,17 +86,24 @@ class S3Counter():
         # Make sure that the correct counters are being held
         prefix_dir = (self.separator + data_object['Key']).rsplit(self.separator, maxsplit=1)[0]
 
-        if self.depth >= 0 and data_object['Key'].count('/') < self.depth:
-            # Show this object in the totals
-            # print('Displaying object {}.'.format(data_object['Key']))
-            self.report(Prefix(data=data_object))
-        elif self._compare_prefixes(self.current_prefix, prefix_dir):
-            # Don't show the object just count it
-            # print('Counting object {}.'.format(data_object['Key']))
+        if self.depth >= 0 and data_object['Key'].count(self.separator) >= self.depth:
+            # Don't show the object just count it (out of depth)
             self.counters[-1].count(data_object)
+        elif self._compare_prefixes(self.current_prefix, prefix_dir):
+            # Current prefix is set correctly, display this object
+            if self.counters[-1].number_objects >= self.limit:
+                # Do not display this object, just count it (truncate long list)
+                self.counters[-1].count(data_object)
+            else:
+                # Display and count object
+                counter = Prefix(data=data_object)
+                self.counters[-1] = self.counters[-1] + counter
+                self.report(counter)
+                if self.counters[-1].number_objects >= self.limit:
+                    self.report_omission()
         else:
             # Current prefix is not correct, adjust it
-            print('Adjusting prefixes. Current {}, new {}'.format(self.current_prefix, prefix_dir))
+            # print('Adjusting prefixes. Current {}, new {}'.format(self.current_prefix, prefix_dir))
             prefix_increment = ''
             index = 0
             for prefix_part in prefix_dir.split(self.separator):
@@ -131,20 +143,35 @@ class S3Counter():
                 self.count(data_object)
 
 
-def s3_disk_usage( 
-            Bucket, Depth=float('Inf'), Delimiter="/", Prefix="", #flatten_large_results=True,
+async def count_page(counter, page):
+    counter.count_list(page.get('Contents', []))
+
+
+async def s3_disk_usage( 
+            Bucket, Depth=float('Inf'), Delimiter="/", Prefix="", MaxObjectsToDisplay=10,
             client=boto3.client('s3')
         ):
     # Calculate disk usage within S3 and report back to parent
-    counter = S3Counter(prefix=Prefix, depth=Depth, separator=Delimiter)
+    counter = S3Counter(prefix=Prefix, depth=Depth, separator=Delimiter, limit=MaxObjectsToDisplay)
     try:
         paginator = client.get_paginator('list_objects_v2')
         page_iterator = paginator.paginate(Bucket=Bucket, Prefix=Prefix) # Delimiter=Delimiter
 
+        last_page = {"Contents": []}
+        # t_start = datetime.datetime.now()
+        # n_pages = 0
+        task = asyncio.create_task(count_page(counter, last_page))
         for page in page_iterator:
-            # Do something with the contents of this prefix
-            # Deal with the files
-            counter.count_list(page.get('Contents', []))
+            # Count the files
+            await task
+            last_page = page
+            task = asyncio.create_task(count_page(counter, last_page))
+            # Measure speed. Last measurement - async 3.38 pages / sec on my laptop, sync: 
+            # t_now = datetime.datetime.now()
+            # t_diff = t_now - t_start
+            # n_pages = n_pages + 1
+            # print("{} pages per second".format(n_pages / t_diff.total_seconds()))
+        await task
     except Exception as e:
         print("Exception counting objects in s3://{}/{}".format(Bucket, Prefix))
         print("Exception: {}".format(e))
@@ -181,7 +208,7 @@ def main():
     parser.add_argument("--bucket", type=str, help="S3 bucket name")
     parser.add_argument("--prefix", type=str, help="S3 bucket prefix", default="")
     parser.add_argument("--delimiter", type=str, help="S3 bucket delimiter", default="/")
-    # parser.add_argument("--truncate", type=bool, help="Summarise keys with over 1000 results?", default=True)
+    parser.add_argument("--truncate", type=int, help="Truncate list over N results?", default=5)
     parser.add_argument("--file", "-f", type=str, help="File name to output data to", default="")
 
     args = parser.parse_args()
@@ -200,14 +227,14 @@ def main():
     # with open(output_file, 'w') as f:
     #     f.write("[\n")
         # for statistic in s3_disk_usage( 
-    s3_disk_usage( 
+    asyncio.run(s3_disk_usage( 
         Bucket=args.bucket,
         Depth=depth,
         Delimiter=args.delimiter,
         Prefix=args.prefix,
-        # flatten_large_results=True,
+        MaxObjectsToDisplay=args.truncate,
         client=client
-    )
+    ))
         #     # Write to stdout
         #     if args.human:
         #         size = human_bytes(statistic['Size'])
